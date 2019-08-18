16682fb81a597b61d4a84ebdbcbc005e766c3b0d
Muyu8890 <zhuangzf1989@126.com>
2019-08-18 23:05:07 +0800

    网关工程 init

---
+++ joine4cloud-gateway/src/main/java/com/github/joine4cloud/gateway/Joine4cloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.github.joine4cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Joine4cloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Joine4cloudGatewayApplication.class, args);
+    }
+
+}
