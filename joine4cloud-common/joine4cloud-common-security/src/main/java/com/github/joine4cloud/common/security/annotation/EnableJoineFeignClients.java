5b869a0500f984039963357e15f1eca60d4c9f7c
Muyu8890 <zhuangzf1989@126.com>
2019-08-27 21:53:47 +0800

    新增模块

---
+++ joine4cloud-common/joine4cloud-common-security/src/main/java/com/github/joine4cloud/common/security/annotation/EnableJoineFeignClients.java
@@ -0,0 +1,13 @@
+package com.github.joine4cloud.common.security.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by Zhuang on 2019/8/27.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+//@EnableFeignClients
+public @interface EnableJoineFeignClients {
+}
